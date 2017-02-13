ca7466c115b1093b041d4ba900c10c751543a555
swaggy <stas.p@hotger.com>
2017-02-13 14:22:13 +0300

    clean up

--- app/src/main/java/com/medeuz/customkeyboard/SimpleIME.java
+++ app/src/main/java/com/medeuz/customkeyboard/SimpleIME.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
-import android.widget.Toast;
 
 
 public class SimpleIME extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
